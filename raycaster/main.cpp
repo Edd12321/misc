38c821ed3cb53e0dfe382dbb050618f1054efde5
Edd12321 <sanduedi309@gmail.com>
2025-10-08 22:39:08 +0300

    Fix fisheye stuff

--- raycaster/main.cpp
+++ raycaster/main.cpp
@@ -11,31 +11,31 @@
 #include <unistd.h>
 
 struct Map {
-	std::vector<std::string> grid;
+	std::vector<std::wstring> grid;
 	size_t width = 0, height = 0;
 };
 
-float x = -1, y = -1, o;
+double x = -1, y = -1, o;
 struct winsize ws;
 
 Map read_map(std::string const& filename)
 {
 	Map ret_val{};
-	std::ifstream fin(filename);
+	std::wifstream fin(filename);
 	if (!fin) {
 		perror(filename.c_str());
 		return ret_val;
 	}
-	std::string buf;
+	std::wstring buf;
 	while (std::getline(fin, buf)) {
 		size_t f;
-		if ((f = buf.find('*')) != std::string::npos) {
+		if ((f = buf.find(L'*')) != std::wstring::npos) {
 			if (x == -1 && y == -1) {
 				buf[f] = ' ';
 				x = f;
 				y = ret_val.height;
 			} else {
-				std::cerr << "map must contain only one *\n";
+				std::wcerr << L"map must contain only one *\n";
 				return ret_val;
 			}
 		}
@@ -44,7 +44,7 @@ Map read_map(std::string const& filename)
 		ret_val.grid.push_back(buf);
 	}
 	if (x == -1 && y == -1) {
-		std::cerr << "map must contain * for player spawn\n";
+		std::wcerr << L"map must contain * for player spawn\n";
 		return ret_val;
 	}
 	for (auto& it : ret_val.grid)
@@ -58,9 +58,9 @@ void play_map(Map const& map)
 	if (x == -1 && y == -1)
 		return;
 
-	constexpr float pi = 3.1415926535;
+	bool t = false, g = true, f = false; // not textured, show lighting
 
-	auto clamp = [&](float& x, float& y)
+	auto clamp = [&](double& x, double& y)
 	{
 		if ((int)x < 0) x = 0;
 		if ((int)x >= map.width) x = map.width - 1;
@@ -69,35 +69,38 @@ void play_map(Map const& map)
 	};
 
 	for (;;) {
-		float step = pi / ws.ws_col;
+		double step = M_PI/2 / ws.ws_col;
 		int k = 0;
-		std::vector<std::string> buf(ws.ws_row, std::string(ws.ws_col, ' '));
-		for (float angle = o + pi / 2; angle >= o - pi / 2; angle -= step) {
-			if (k++ >= ws.ws_col)
-				break;
-			float xx = x, yy = y;
-			float sina = sin(angle), cosa = cos(angle);
-			while (xx >= 0 && xx < map.width && yy >= 0 && yy < map.height && isspace(map.grid[yy][xx]))
+		std::vector<std::wstring> buf(ws.ws_row, std::wstring(ws.ws_col, ' '));
+
+		// Floor
+		if (g)
+			for (size_t i = ws.ws_row / 2 + 1; i < ws.ws_row; ++i)
+				buf[i] = std::wstring(ws.ws_col, ',');
+
+		for (double angle = o + M_PI / 4; k < ws.ws_col && angle >= o - M_PI / 4; angle -= step, ++k) {
+			double xx = x, yy = y;
+			double sina = sin(angle), cosa = cos(angle);
+			while (xx >= 0 && xx < map.width && yy >= 0 && yy < map.height && iswspace(map.grid[yy][xx]))
 				xx += 0.1 * cosa, yy += 0.1 * sina;
-			float dist = sqrt(pow(xx - x, 2) + pow(yy - y, 2));
+			double dist = sqrt(pow(xx - x, 2) + pow(yy - y, 2)) * (f ? 1 : cos(angle - o));
 			int wall_height = ws.ws_row;
 			if (dist >= 1)
 				wall_height /= dist;
 			clamp(xx, yy);
-			char texture = map.grid[yy][xx];
+			wchar_t texture = t ? map.grid[yy][xx] : L"█▓▒░:. "[std::min(int(dist / 6), 6)];
 			for (int i = (ws.ws_row - wall_height) / 2; i < (ws.ws_row + wall_height) / 2; ++i)
 				buf[i][k] = texture;
 		}
 		for (int i = 0; i < ws.ws_row; ++i)
-			std::cout << buf[i] << '\n';
-		std::cout << "\033[0;0H";
+			std::wcout << buf[i] << '\n';
+		std::wcout << L"\033[0;0H";
 
 		char c = getchar();
-		float new_x, new_y, mult = 0.2;
+		double new_x, new_y, mult = 0.2;
 		switch (c) {
 			case 'q': return;
-			case 's':
-				mult *= -1; // fallthrough
+			case 's': mult *= -1; // fallthrough
 			case 'w':
 				new_x = x+mult*cos(o);
 				new_y = y+mult*sin(o);
@@ -105,10 +108,13 @@ void play_map(Map const& map)
 					x = new_x, y = new_y;
 				clamp(x, y);
 				break;
-			case 'd':
-				mult *= -1; // fallthrough
+			case 'd': mult *= -1; // fallthrough
 			case 'a':
-				o += mult; o = fmod(o, 2*pi); break;
+				o += mult; o = fmod(o, 2*M_PI);
+				break;
+			case 't': t = !t; break;
+			case 'g': g = !g; break;
+			case 'f': f = !f; break;
 		}
 	}
 }
@@ -120,8 +126,9 @@ void get_ws(int sig = 0)
 
 int main(int argc, char *argv[])
 {
+	setlocale(LC_ALL, "en_US.UTF-8");
 	if (argc < 2) {
-		std::cerr << "usage: " << argv[0] << " <map>\n";
+		std::wcerr << L"usage: " << argv[0] << L" <map>\n";
 		return 1;
 	}
 
@@ -136,9 +143,9 @@ int main(int argc, char *argv[])
 	newt = oldt;
 	newt.c_lflag &= ~(ICANON | ECHO);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &newt);
-	std::cout << "\033[?25l";
+	std::wcout << L"\033[?25l";
 	play_map(read_map(argv[1]));
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &oldt);
-	std::cout << "\033[2J\033[?25h";
+	std::wcout << L"\033[2J\033[?25h";
 	return 0;
 }
